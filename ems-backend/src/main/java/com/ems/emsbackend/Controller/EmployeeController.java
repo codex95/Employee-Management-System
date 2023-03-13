ea776697756c839663f5020f5777a694cb76f744
codex95 <afiqhamdan95@gmail.com>
2023-03-14 00:42:56 +0800

    Add @Autowired to employeerepository to fix null pointer exception error

--- ems-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
+++ ems-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.ems.emsbackend.Controller;
 
 import com.ems.emsbackend.Model.Employee;
 import com.ems.emsbackend.Repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 public class EmployeeController {
 
     // Inject repository
+    @Autowired
     private EmployeeRepository employeeRepository;
 
     // Get all employees
