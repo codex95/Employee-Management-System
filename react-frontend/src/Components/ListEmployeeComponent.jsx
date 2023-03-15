ca4eb5b61f8328bcca5ea9e538a7abb89d3f5219
codex95 <afiqhamdan95@gmail.com>
2023-03-15 19:49:48 +0800

    Create EmployeeService object to call getEmployees API

--- react-frontend/src/Components/ListEmployeeComponent.jsx
+++ react-frontend/src/Components/ListEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import EmployeeService from '../Services/EmployeeService';
 
 class ListEmployeeComponent extends Component {
  constructor(props) {
@@ -7,6 +8,12 @@ class ListEmployeeComponent extends Component {
     this.state = {
         employees: []
     }
+ }
+
+ componentDidMount() {
+    EmployeeService.getEmployees().then((res) => {
+        this.setState({ employees: res.data});
+    });
  }
 
  render() {
--- react-frontend/src/Services/EmployeeService.js
+++ react-frontend/src/Services/EmployeeService.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+const EMPLOYEE_API_BASE_URL = 'http://localhost:8080/api/v1/employees';
+
+// Create EmployeeService object to call getEmployees API
 class EmployeeService {
+    
+    getEmployees() {
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
 
 }
+
+export default new EmployeeService();
