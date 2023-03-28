af9f445e164ec246218669be2d0dd49e57ba7cc1
codex95 <afiqhamdan95@gmail.com>
2023-03-28 15:22:30 +0800

    Change react-router-dom to v5.2

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import ListEmployeeComponent from './Components/ListEmployeeComponent';
 import HeaderComponent from './Components/HeaderComponent';
 import FooterComponent from './Components/FooterComponent';
@@ -7,13 +8,17 @@ import FooterComponent from './Components/FooterComponent';
 function App() {
   return (
     <div>
-      <HeaderComponent />
-      <div className="container">
-        <ListEmployeeComponent />
-      </div>
-      <div>
-        <FooterComponent />
-      </div>
+      <Router>
+          <HeaderComponent />
+            <div className="container">
+              <Switch>
+                 <Route path="/" component={ListEmployeeComponent}></Route>
+                 <Route path="/employees" component={ListEmployeeComponent}></Route>
+                 <ListEmployeeComponent />
+              </Switch>
+            </div>
+          <FooterComponent />
+      </Router>  
     </div>
   );
 }
