ab8967478afc73d097b33ded2447a7d5f779f448
codex95 <afiqhamdan95@gmail.com>
2023-03-28 16:16:39 +0800

    Add create employee rest api

--- spring-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
+++ spring-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.ems.emsbackend.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,11 @@ public class EmployeeController {
 
         return employeeRepository.findAll();
 
+    }
+
+    // Create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
