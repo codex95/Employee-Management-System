e3dd1ba0485ce82380a194459386ca7ae5eb6d66
codex95 <afiqhamdan95@gmail.com>
2023-03-28 14:37:12 +0800

    Change https to http

--- spring-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
+++ spring-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 // Add CrossOrigin annotation to solve CORS policy issue
-@CrossOrigin(origins = "https://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
