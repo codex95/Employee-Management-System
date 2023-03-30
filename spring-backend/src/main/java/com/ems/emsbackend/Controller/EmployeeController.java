e9b6589b655f0d80bf24bc4a60477ea55ab92a30
codex95 <afiqhamdan95@gmail.com>
2023-03-30 11:33:09 +0800

    Create get employee by id rest api

--- spring-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
+++ spring-backend/src/main/java/com/ems/emsbackend/Controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package com.ems.emsbackend.Controller;
 
+import com.ems.emsbackend.Exception.ResourceNotFoundException;
 import com.ems.emsbackend.Model.Employee;
 import com.ems.emsbackend.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,5 +36,13 @@ public class EmployeeController {
     @PostMapping("/employees")
     public Employee createEmployee(@RequestBody Employee employee) {
         return employeeRepository.save(employee);
+    }
+
+    // Get employee by Id rest api
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with id: " + id));
+        return ResponseEntity.ok(employee);
     }
 }
